97f7f2cc71d0898bd8c2543274669d0c2d1c61c7
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-05-20 03:24:45 +0600

    css file join

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def store(request):
+    return render(request,'store.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
