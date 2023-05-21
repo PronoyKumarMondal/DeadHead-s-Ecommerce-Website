60ab8bafff152467b772b7c4c9f4db864ce97c58
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-05-22 04:11:32 +0600

    image problem solved js addded json imported

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 # Create your views here.
@@ -7,7 +9,41 @@ def store(request):
     context= {'products': products}
     return render(request,'store.html',context)
 
+
 def cart(request):
-    return render(request,'cart.html')
+    
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order={'get_cart_total':0,'get_cart_items':0}
+        
+    context= {'items': items, 'order':order}    
+    return render(request,'cart.html',context)
+
+
 def checkout(request):
-    return render(request,'checkout.html')
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order={'get_cart_total':0,'get_cart_items':0}
+        
+    context= {'items': items, 'order':order}  
+    return render(request,'checkout.html',context)
+
+def updateItem(request):
+    data= json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:',action)
+    print('Product:',productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order,created= Order.objects.get_or_create(customer=customer,complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+    return JsonResponse('Item was added',safe=False)
