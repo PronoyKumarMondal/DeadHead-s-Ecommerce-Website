f68f5a51a3701059b311e5db50e0cffc4c6da446
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-05-21 06:41:30 +0600

    database added to store but it has 1 problem it cannot access items

--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def store(request):
-    return render(request,'store.html')
+    products = Product.objects.all()
+    context={'products':products}
+    return render(request,'store.html',context)
+
 def cart(request):
     return render(request,'cart.html')
 def checkout(request):
