85a72f3e126912dec57a9a4b3b04797e2fffa154
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-05-30 00:33:58 +0600

    login singup page added cart problem solved search design added without backend footer added without backend dropdown added without backend etc

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path('checkout/',views.checkout,name="checkout"),
     path('update_item/',views.updateItem,name="update_item"),
     path('process_order/',views.processOrder,name="process_order"),
+    path('singup/',views.singup,name="singup"),
+    path('login/',views.loginPage,name="login"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
+from django.http import HttpResponse
 import json
 import datetime
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import inlineformset_factory
+from .forms import OrderForm,CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from . utils import cookieCart,cartData,guestOrder
 
 # Create your views here.
-
+def singup(request):
+    form = CreateUserForm()
+    
+    if request.method=="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user=form.cleaned_data.get('username')
+            customer = request.user
+            customer, created = Customer.objects.get_or_create(
+            customer=customer, complete=False)
+            messages.success(request,'Account was created for '+user)
+            return redirect('login')
+        
+    context ={'form':form}
+        
+    return render(request,'singup.html',context)
+def loginPage(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            messages.info(request, 'Username OR Password is incorret')
+        
+        
+    context = {}
+    return render(request,'login.html', context)
 
 def store(request):
 
@@ -71,9 +109,9 @@ def processOrder(request):
     
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(
-            customer=customer, complete=False)
-        
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order_items = OrderItem.objects.filter(order__customer=request.user.customer, order__complete=False)
+        order_items.delete()
         
     else:
         customer,order = guestOrder(request,data) 
